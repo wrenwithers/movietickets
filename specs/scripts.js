26d08de5ea1982b4238941b2fe2702e417a3b550
Wren Withers <wrenwithers@Wrens-MacBook-Air.local>
2016-01-27 07:48:36 -0800

    start to get there with the js

--- specs/scripts.js
+++ specs/scripts.js
@@ -8,3 +8,22 @@ function Ticket(movie, time, age, price) {
 Ticket.prototype.ticketPrice = function() {
   return this.movie + ", " + this.time + ", " + this.age + " : " + this.price;
 }
+
+$(document).ready(function() {
+  $("form#new-ticket").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedMovie = $("input#new-movie").val();
+    var inputtedTime = $("input#new-time").val();
+    var inputtedAge = $("input#new-age").val();
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+  });
+});
+
+
+$('.dropdown-toggle').dropdown()
